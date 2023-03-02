5a5ac607c404b8d0bd16e91f38c716f21c87f01b
Sarah Raguin <sarahraguin@gmail.com>
2023-03-02 16:09:56 -0500

    Still working on algo 2

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -136,16 +136,121 @@ function retrieveInputSearch() {
 }
 
 function lookInRecipesArray() {
+    let newRecipesArray = [];
     let recipesArray = recipes;
     let inputSearch = retrieveInputSearch();
     let resultForName = recipesArray.filter(recipe => recipe.name.toLowerCase().includes(inputSearch));
+    newRecipesArray.push(resultForName);
     let resultForDescription = recipesArray.filter(recipe => recipe.description.toLowerCase().includes(inputSearch));
-    let resultForIngredients = recipesArray.filter(recipe => {
-        recipe.ingredients.filter(item => item.ingredient.includes(inputSearch));
-    });
-    console.log(resultForName);
-    console.log(resultForDescription);
-    console.log(resultForIngredients);
+    newRecipesArray.push(resultForDescription);
+    let resultForIngredients = recipesArray.filter(recipe =>
+        recipe.ingredients.some(item => item.ingredient.includes(inputSearch))
+    // some is called on ingredients array and check if at least one ingredient includes the inputSearch = true = included in filtered array
+    );
+    newRecipesArray.push(resultForIngredients);
+    let combinedArray = [];
+    combinedArray.push(...newRecipesArray[0], ...newRecipesArray[1], ...newRecipesArray[2]);
+    let combinedUniqueArray = [...new Set(combinedArray)];
+    return combinedUniqueArray;
 }
 
-lookInRecipesArray();
+function retrieveNewAppliance() {
+    let applianceArray = [];
+    let recipesArray = lookInRecipesArray();
+    applianceArray = recipesArray.filter(recipe => recipe.appliance)
+    .map(recipe => recipe.appliance);
+    return applianceArray;
+}
+
+function retrieveNewUstensils() {
+    let ustensilsArray = [];
+    let recipesArray = lookInRecipesArray();
+    ustensilsArray = recipesArray.filter(recipe => recipe.ustensils)
+    .map(recipe => recipe.ustensils);
+    return ustensilsArray;
+}
+
+function retrieveNewIngredients() {
+    let ingredientsArray = [];
+    let recipesArray = lookInRecipesArray();
+    ingredientsArray = recipesArray.filter(recipe => recipe.ingredients.ingredient)
+    .map(recipe => recipe.ingredients.ingredient);
+    return ingredientsArray;
+}
+
+function addOptionsToSelectNewAppliance() {
+    let applianceArray = retrieveNewAppliance();
+    const select = document.getElementById("appliance");
+    let firstOption = new Option("Appareils", "");
+    firstOption.setAttribute("disabled", "");
+    firstOption.setAttribute("selected", "");
+    select.appendChild(firstOption, undefined);
+    for(index in applianceArray) {
+        // New variable option which stock new option from array
+        let option = new Option(applianceArray[index], index);
+        // then adding at the end of the list the option created
+        select.options[select.options.length] = option;
+    }
+}
+
+function addOptionsToSelectNewUstensils() {
+    let ustensilsArray = retrieveNewUstensils();
+    const select = document.getElementById("ustensils");
+    let firstOption = new Option("Ustensiles", "");
+    firstOption.setAttribute("disabled", "");
+    firstOption.setAttribute("selected", "");
+    select.appendChild(firstOption, undefined);
+    for(index in ustensilsArray) {
+        // New variable option which stock new option from array
+        let option = new Option(ustensilsArray[index], index);
+        // then adding at the end of the list the option created
+        select.options[select.options.length] = option;
+    }
+}
+
+function addOptionsToSelectNewIngredients() {
+    let ingredientsArray = retrieveNewIngredients();
+    const select = document.getElementById("ingredients");
+    let firstOption = new Option("Ingrédients", "");
+    firstOption.setAttribute("disabled", "");
+    firstOption.setAttribute("selected", "");
+    select.appendChild(firstOption, undefined);
+    for(index in ingredientsArray) {
+        // New variable option which stock new option from array
+        let option = new Option(ingredientsArray[index], index);
+        // then adding at the end of the list the option created
+        select.options[select.options.length] = option;
+    }
+}
+
+function retrieveNewTags() {
+    addOptionsToSelectNewAppliance();
+    addOptionsToSelectNewUstensils();
+    addOptionsToSelectNewIngredients();
+}
+
+function removeAll(selectBox) {
+    while (selectBox.options.length > 0) {
+        selectBox.remove(0);
+    }
+}
+
+function displayNewData() {
+    let container = document.querySelector(".recipeSection");
+    let childElement = document.querySelector(".container.d-flex.flex-wrap.gap-5.justify-content-between");
+    container.removeChild(childElement);
+    const recipe = lookInRecipesArray();
+    const recipeSection = document.querySelector(".recipeSection");
+    const recipeModel = recipesFactory(recipe);
+    const recipeDOM = recipeModel.createDOM();
+    recipeSection.appendChild(recipeDOM);
+    let containerOfOptionsIngredients = document.getElementById("ingredients");
+    removeAll(containerOfOptionsIngredients);
+    let containerOfOptionsAppliance = document.getElementById("appliance");
+    removeAll(containerOfOptionsAppliance);
+    let containerOfOptionsUstensils = document.getElementById("ustensils");
+    removeAll(containerOfOptionsUstensils);
+    retrieveNewTags();
+}
+
+document.getElementById("search").addEventListener("keyup", displayNewData);
